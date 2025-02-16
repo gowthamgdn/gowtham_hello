ccb95ebed253ce8cf67a24c108ea443fe53159ae
Sai Gowtham <78816361+gowthamgdn@users.noreply.github.com>
2025-02-17 00:55:27 +0530

    Update main.py

--- testing/main.py
+++ testing/main.py
@@ -1,2 +1,5 @@
-from gowtham_hello import hello
-hello()
+from gowtham_hello.main import get_quote
+
+# Call the function and print the motivational quote
+print("Testing gowtham_hello package:")
+print(get_quote())
