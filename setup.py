33fabcdc2df585e7a84ad3b1a0d99f602c0c90c0
Sai Gowtham <78816361+gowthamgdn@users.noreply.github.com>
2025-02-16 23:22:24 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gowtham_hello',
+    version='0.3',
+    pacakages=find_packages(),
+    install_requries=[
+        'numpy>=2.0'
+    ],
+)
